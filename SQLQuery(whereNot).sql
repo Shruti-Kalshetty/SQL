5e4e44704a9b826b6c9fa456915fe9b8091729d4
Shruti Rajkumar Kalshetty <shrutikalshetty07@gmail.com>
2024-11-21 13:39:39 +0530

    Add files via upload

---
+++ SQLQuery(whereNot).sql
@@ -0,0 +1,13 @@
+use employee;
+
+select * from employee;
+
+select * from employee
+where not Salary=50000;
+
+select * from employee;
+
+select * from employee
+where not EmpName='Manya';
+
+select * from employee;
