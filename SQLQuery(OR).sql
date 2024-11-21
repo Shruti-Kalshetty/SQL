f0890ef66918570163aa983d3c68f3be2879b3e4
Shruti Rajkumar Kalshetty <shrutikalshetty07@gmail.com>
2024-11-21 13:38:45 +0530

    Add files via upload

---
+++ SQLQuery(OR).sql
@@ -0,0 +1,13 @@
+use employee;
+
+select * from employee;
+
+select * from employee
+where EmpName='Avnish' or EmpName='Navya';
+
+select * from employee;
+
+select * from employee
+where EmpName='kavita' or EmpName='nandita';
+
+select * from employee;
